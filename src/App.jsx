70956e0b1e77953a5ceac2d396005e8577fa3aaf
Kemal Kazanc <keemalkazanc@gmail.com>
2024-01-23 14:37:40 +0100

    mise en place de l'api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
 import loader from "../src/assets/loader.svg";
 import "./App.css";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const APIKEY = import.meta.env.VITE_WEATHER_API_KEY;
+  useEffect(() => {
+    fetch(
+      `http://api.airvisual.com/v2/nearest_city?lat={{LATITUDE}}&lon={{LONGITUDE}}&key=${APIKEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setWeatherData({
+          city: data.data.city,
+          country: data.data.country,
+          iconId: data.data.current.weather.ic,
+          temperature: data.data.current.weather.tp,
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        alert("Error");
+      });
+  }, []);
+
   return (
     <main>
       <div className="loader-container">
